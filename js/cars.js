5130839868c1f6e698ee2eab49541eaa57597ab3
Yaroslav <tsyarosalv2007@gmail.com>
2024-03-01 14:38:24 +0000

    project init

---
+++ js/cars.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  let services = prompt("What services are you interested in?", "I want to buy a new car!");
+  if (services == null || services == "") {
+    txt = "No name provided";
+  } else {
+    txt ="We have received your request: " + services + " Thank you!";
+  }
+  alert(txt);
+}
